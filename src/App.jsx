8809ce516f5f1aa009950ba11c9961e19491c5cc
Maryna Binko <benyhorses@hotmail.com>
2024-06-04 14:29:15 +0100

    cast picture added

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 
 import './App.css'
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import { lazy, Suspense } from "react";
 import Navigation from "./components/Navigation/Navigation";
 
--- src/components/MovieCast/MovieCast.jsx
+++ src/components/MovieCast/MovieCast.jsx
@@ -1,5 +1,6 @@
 // import styles from "./MovieCast.module.css"
 
+
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { fetchMovieCredits } from "../../movies-api";
@@ -17,7 +18,22 @@ const MovieCast = () => {
       <h2>Cast</h2>
       <ul>
         {cast.map(actor => (
-          <li key={actor.cast_id}>{actor.name}</li>
+          <li key={actor.cast_id}>
+            {actor.profile_path ? (
+              <img
+                src={`https://image.tmdb.org/t/p/w200${actor.profile_path}`}
+                alt={actor.name}
+                width="100"
+              />
+            ) : (
+              <img
+                src={`https://via.placeholder.com/100?text=No+Image`}
+                alt="No Image"
+                width="100"
+              />
+            )}
+            <p>{actor.name}</p>
+          </li>
         ))}
       </ul>
     </div>

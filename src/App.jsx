25bc6f924c70bf6e67aff02a037a9af5d2170598
Maryna Binko <benyhorses@hotmail.com>
2024-06-04 11:14:14 +0100

    pages added

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,30 @@
 
 import './App.css'
+import HomePage from "./pages/HomePage/HomePage"
+import MoviesPage from "./pages/MoviesPage/MoviesPage"
+import MovieDetailsPage from "./pages/MovieDetailsPage/MovieDetailsPage"
+import NotFoundPage from "./pages/NotFoundPage/NotFoundPage"
+import MovieCast from "./components/MovieCast/MovieCast"
+import MovieReviews from "./components/MovieReviews/MovieReviews"
+import { Routes, Route } from "react-router-dom";
+import Navigation from './components/Navigation/Navigation'
 
 function App() {
 
 
   return (
     <>
-   
+    <Navigation/>
+     <Routes>
+     <Route path="/" element={<HomePage />} />
+     <Route path="/moviesPage" element={<MoviesPage />} />
+     <Route path="/moviedetailspage" element={<MovieDetailsPage />} />
+     <Route path="/moviedetailspage/:moviecast" element={<MovieCast />} />
+     <Route path="/moviedetailspage/:moviereviews" element={<MovieReviews />} />
+
+     <Route path="*" element={<NotFoundPage />} />
+    </Routes>
+    
     </>
   )
 }
--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,10 +1,14 @@
 // import styles from "./Navigation.module.css"
+import { Link } from "react-router-dom";
+
 
 const Navigation = () => {
   return (
-    <div>
-      
-    </div>
+<nav>
+  <Link to="/">HomePage</Link>
+  <Link to="/moviespage">MoviesPage</Link>
+  <Link to="/moviedetailspage">MovieDetailsPage</Link>
+</nav>
   )
 }
 

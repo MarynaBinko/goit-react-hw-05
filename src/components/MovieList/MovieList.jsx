a038813e0a1045bcba399a7ab5febb90b25e45a7
Maryna Binko <benyhorses@hotmail.com>
2024-06-04 10:46:43 +0100

    react router installed

---
+++ src/components/MovieList/MovieList.jsx
@@ -0,0 +1,11 @@
+// import styles from "./MovieList.module.css"
+
+const MovieList = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default MovieList
---
+++ src/components/Navigation/Navigation.jsx
@@ -0,0 +1,11 @@
+// import styles from "./Navigation.module.css"
+
+const Navigation = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Navigation

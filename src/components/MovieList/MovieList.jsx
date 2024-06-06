6c56bb24cc0acaaa640cc5c356a9e97def59c895
Maryna Binko <benyhorses@hotmail.com>
2024-06-06 11:09:37 +0100

    movieeList use location added

--- src/components/MovieList/MovieList.jsx
+++ src/components/MovieList/MovieList.jsx
@@ -1,16 +1,22 @@
 import styles from "./MovieList.module.css"
 
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 
-const MovieList = ({ movies }) => (
-  <ul className={styles.list}>
-    {movies.map(movie => (
-      <li key={movie.id} className={styles.listItem}>
-        <Link to={`/movies/${movie.id}`}>{movie.title}</Link>
-      </li>
-    ))}
-  </ul>
-);
+
+const MovieList = ({ movies }) => {
+  const location = useLocation();
+  return (
+    <ul className={styles.list}>
+      {movies.map(movie => (
+        <li key={movie.id} className={styles.listItem}>
+          <Link to={`/movies/${movie.id}`} state={{ from: location }}>{movie.title}</Link>
+        </li>
+      ))}
+    </ul>
+  );
+};
+
+
 
 export default MovieList;
 
